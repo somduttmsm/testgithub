8daa0d637941be300b95c157ca93bfe18e401e1e
aman <somdutt.msm@gmail.com>
2023-05-16 14:48:15 +0530

    Added new welcome feature

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ import export from "./export.json";
 // fetch("./export.json").then((response) => console.log(response));
 console.log(export)
 console.log("aman");
+const newFeature=function(){
+    console.log("welcome to the application")
+}
+newFeature()
 init();
